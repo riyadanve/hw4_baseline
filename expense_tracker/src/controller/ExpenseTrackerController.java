c5f30abd61de647b5e73f900a97bf40673a82ec9
riyadanve <86950420+riyadanve@users.noreply.github.com>
2023-12-04 16:29:57 -0500

    Updated the controller to follow observer design pattern

--- expense_tracker/src/controller/ExpenseTrackerController.java
+++ expense_tracker/src/controller/ExpenseTrackerController.java
@@ -1,58 +1,44 @@
 package controller;
 
 import view.ExpenseTrackerView;
+import model.ExpenseTrackerModel;
+import model.Transaction;
+import model.Filter.TransactionFilter;
+import model.ExpenseTrackerModelListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JOptionPane;
 
-import model.ExpenseTrackerModel;
-import model.Transaction;
-import model.Filter.TransactionFilter;
-
-public class ExpenseTrackerController {
+public class ExpenseTrackerController implements ExpenseTrackerModelListener {
   
   private ExpenseTrackerModel model;
   private ExpenseTrackerView view;
-  /** 
-   * The Controller is applying the Strategy design pattern.
-   * This is the has-a relationship with the Strategy class 
-   * being used in the applyFilter method.
-   */
   private TransactionFilter filter;
 
   public ExpenseTrackerController(ExpenseTrackerModel model, ExpenseTrackerView view) {
     this.model = model;
     this.view = view;
-    // For the MVC architecture pattern, the Observer design pattern is being
-    // used to update the View after manipulating the Model.
-    this.model.register(this.view);
+    this.model.register(this); // Register the controller as a listener to the model
   }
 
   public void setFilter(TransactionFilter filter) {
-    // Sets the Strategy class being used in the applyFilter method.
     this.filter = filter;
   }
 
   public boolean addTransaction(double amount, String category) {
-    if (!InputValidation.isValidAmount(amount)) {
-      return false;
-    }
-    if (!InputValidation.isValidCategory(category)) {
+    if (!InputValidation.isValidAmount(amount) || !InputValidation.isValidCategory(category)) {
       return false;
     }
     
     Transaction t = new Transaction(amount, category);
     model.addTransaction(t);
-    view.update(model);
     return true;
   }
 
   public void applyFilter() {
-    //null check for filter
-    if(filter!=null){
-      // Use the Strategy class to perform the desired filtering
+    if (filter != null) {
       List<Transaction> transactions = model.getTransactions();
       List<Transaction> filteredTransactions = filter.filter(transactions);
       List<Integer> rowIndexes = new ArrayList<>();
@@ -63,25 +49,23 @@ public class ExpenseTrackerController {
         }
       }
       model.setMatchedFilterIndices(rowIndexes);
-      view.update(model);
-    }
-    else{
+    } else {
       JOptionPane.showMessageDialog(view, "No filter applied");
-      view.toFront();}
-
+      view.toFront();
+    }
   }
 
-  //for undoing any selected transaction
   public boolean undoTransaction(int rowIndex) {
     if (rowIndex >= 0 && rowIndex < model.getTransactions().size()) {
       Transaction removedTransaction = model.getTransactions().get(rowIndex);
       model.removeTransaction(removedTransaction);
-      view.update(model);
-      // The undo was allowed.
       return true;
     }
-
-    // The undo was disallowed.
     return false;
-  }    
+  }
+
+  // Implement the method from ExpenseTrackerModelListener
+  public void update(ExpenseTrackerModel model) {
+    view.update(model); // Notify the view about the model changes
+  }
 }
